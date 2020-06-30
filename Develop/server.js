583dab86a6dfd2af5f88c4b4c6dc3ad15c05d4de
Kaylie V <k.verner37@gmail.com>
2020-06-30 15:16:22 -0400

    updating connection to mongodb for heroku deployment

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+
+mongoose.connect(MONGODB_URI)
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,7 +18,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 require("./routes/api.js")(app);
 
